cd0c03f02edcc8aa0c89c3513f57cc72dbf98467
Maximilian Horst <maximilian.horst@hhl.de>
2021-03-09 19:32:23 +0100

    mentorlist

---
+++ client/src/components/mentee/MentorDetail.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class MentorDetail extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Mentor Detail</h1>
+      </div>
+    )
+  }
+}
--- client/src/components/mentee/MentorList.js
+++ client/src/components/mentee/MentorList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import MentorDetail from './MentorDetail'
 
 export default class MentorList extends Component {
 
@@ -7,7 +8,8 @@ state = {
   user: this.props.user,
   allMentors: null,
   // preferredMentors: [],
-  error: null
+  error: null,
+  detailView: null
 }
 
 componentDidMount() {
@@ -37,23 +39,56 @@ getMentors = () => {
     })
   }
 
-  likeMentor = event => {
-    console.log(event)
+  likeMentor = mentorId => {
+    console.log(this.props.user.preferredMentors)
+    if (this.props.user.preferredMentors.includes(mentorId)) {
+    console.log(mentorId)
     console.log(this.state.user._id)
-    axios.put(`/api/mentee/mentor-list/${this.state.user._id}`, {
-     preferredMentor: event })
-     .then(response => {
-       this.setState({
-        preferredMentors: response.data.preferredMentors
-       })
-       //update the setUser in App.js after the prefrred mentor array gets updated
-       this.props.setUser(response.data)
-      //  console.log(this.state.preferredMentors)
+    axios.put(`/api/mentee/mentor-list/${this.state.user._id}/unlike`, {
+      preferredMentor: mentorId })
+      .then(response => {
+        this.setState({
+         preferredMentors: response.data.preferredMentors
+        })
+        //update the setUser in App.js after the prefrred mentor array gets updated
+        this.props.setUser(response.data)
+       //  console.log(this.state.preferredMentors)
+      })
+      .catch(err => {
+       console.log(err)
      })
-     .catch(err => {
-      console.log(err)
-    })
+    
+    } else {
+      axios.put(`/api/mentee/mentor-list/${this.state.user._id}/like`, {
+        preferredMentor: mentorId })
+        .then(response => {
+          this.setState({
+           preferredMentors: response.data.preferredMentors
+          })
+          //update the setUser in App.js after the prefrred mentor array gets updated
+          this.props.setUser(response.data)
+         //  console.log(this.state.preferredMentors)
+        })
+        .catch(err => {
+         console.log(err)
+       })
+  
+    }
   }
+
+  detailView = (mentorId) => {
+    this.setState({
+      detailView: mentorId
+    })
+      
+    if (this.state.detailViewSelector) { 
+      <MentorDetail
+        {...this.props} 
+      />
+      }
+  }
+
+ 
 
   render() {
     console.log(this.props.user)
@@ -72,6 +107,8 @@ getMentors = () => {
           <img style = {{width: "200px"}} src={mentor.imgPath} alt="userPhoto"/>
           <h3>{mentor.username}</h3>
           <button onClick={() => {this.likeMentor(mentor._id)}}> {this.props.user.preferredMentors.includes(mentor._id) ? "Unlike" : "Like"} </button>
+          <button onClick={() => {this.detailView(mentor._id)}}>Details</button>
+        
           </div>
 
         )
